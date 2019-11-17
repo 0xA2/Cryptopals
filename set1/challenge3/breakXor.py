7a1d70573951824d0e18d6e2b54c8864750a94b5
0xA2 <36355541+0xA2@users.noreply.github.com>
2019-11-17 23:24:34 +0000

    Add files via upload

---
+++ set1/challenge3/breakXor.py
@@ -0,0 +1,60 @@
+import binascii
+
+freqs = {
+    'A': 0.0651738,
+    'B': 0.0124248,
+    'C': 0.0217339,
+    'D': 0.0349835,
+    'E': 0.1041442,
+    'F': 0.0197881,
+    'G': 0.0158610,
+    'H': 0.0492888,
+    'I': 0.0558094,
+    'J': 0.0009033,
+    'K': 0.0050529,
+    'L': 0.0331490,
+    'M': 0.0202124,
+    'N': 0.0564513,
+    'O': 0.0596302,
+    'P': 0.0137645,
+    'Q': 0.0008606,
+    'R': 0.0497563,
+    'S': 0.0515760,
+    'T': 0.0729357,
+    'U': 0.0225134,
+    'V': 0.0082903,
+    'W': 0.0171272,
+    'X': 0.0013692,
+    'Y': 0.0145984,
+    'Z': 0.0007836,
+    ' ': 0.1918182 
+}
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+valid = letters + letters.lower() + " "
+
+def singlebytexor(s,byte):
+	return "".join(chr(ord(s[i])^byte) for i in range(0,len(s)))
+
+def score(s):
+	points = 0
+	for ch in s:
+		if ch in valid:
+			points += freqs[ch.upper()]
+	return points
+
+def main():
+	with open("3.txt","rb") as f:
+		ct = str(binascii.unhexlify(f.read()))
+		max_score = 0
+		key = 0
+		for i in range(0,256):
+			cur_try = singlebytexor(ct,i)
+			cur_score = score(cur_try)
+			if cur_score > max_score:
+				max_score = cur_score
+				key = i 
+	print singlebytexor(ct,key)
+
+if __name__ == "__main__":
+	main()
