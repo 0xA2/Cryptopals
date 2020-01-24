aa27013fcef12b7c5529572846806d8d90af2908
0xA2 <36355541+0xA2@users.noreply.github.com>
2020-01-24 16:40:50 +0000

    Update AES_ECB.py

--- set1/challenge7/AES_ECB.py
+++ set1/challenge7/AES_ECB.py
@@ -1,9 +1,13 @@
 from Crypto.Cipher import AES
+import base64
 
 def main():
 	with open("7.txt", "r") as file:
 		s = "".join(line[:-1] for line in file)
 	passphrase = "YELLOW SUBMARINE"
 	aes = AES.new(passphrase, AES.MODE_ECB)
-	print aes.decrypt(s.decode("base64"))
-main()
+	print (aes.decrypt(base64.b64decode(s)))
+
+if __name__ == "__main__":
+	main()
+
