719f1db5ba3fc9d263271fc343340abf5ab21630
0xA2 <36355541+0xA2@users.noreply.github.com>
2020-01-24 16:37:15 +0000

    Update hex2base64.py

--- set1/challenge1/hex2base64.py
+++ set1/challenge1/hex2base64.py
@@ -1,4 +1,11 @@
-hex = raw_input("Hex number > ")
-str = hex.decode("hex")
-print "Base64 encoded string > " + str.encode("base64")
+import binascii
+import base64
 
+def hex2b64(s):
+	return base64.b64encode(binascii.unhexlify(s))
+
+def main():
+	print(hex2b64(b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'))
+
+if __name__ == "__main__":
+	main()
