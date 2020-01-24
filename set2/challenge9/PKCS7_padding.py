06cce06ea242a19ac010669791a407961eda1dd5
0xA2 <36355541+0xA2@users.noreply.github.com>
2020-01-24 16:42:35 +0000

    Delete PKCS7_padding.py

--- set2/challenge9/PKCS7_padding.py
+++
@@ -1,7 +0,0 @@
-def main():
-	input_bytes = raw_input("Message to pad > ")
-	if (len(input_bytes))%16 == 0:
-		print "Message length is a multiple of 16, no padding needed"
-	else:
-		print input_bytes.ljust(16*(len(input_bytes)/16)+16,chr((16*(len(input_bytes)/16)+16)-len(input_bytes))).encode("hex")
-main()
